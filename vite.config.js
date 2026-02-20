06fddec145a0e0a8bdcf1162f2ee9c73adf2191d
구혜지 <ioabcoi@etoos.com>
2026-02-20 16:15:32 +0900

    build: add dist for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
+  base: "/Beautiful-Word-Map/",
   plugins: [react()],
 })
